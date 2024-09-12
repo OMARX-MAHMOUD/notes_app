87b55a58c0a9d8a7800d8cbf855c07cac418f9e7
omar mahmoud <omarmahmoudazema@gmail.com>
2024-09-12 15:19:32 +0300

    finish custom sheet

--- lib/widgets/custombuttonsheet.dart
+++ lib/widgets/custombuttonsheet.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'package:notes_app/widgets/custom_textfield.dart';
+import 'package:notes_app/widgets/custombutton.dart';
 
 class AddNoteButtonSheet extends StatelessWidget {
   const AddNoteButtonSheet({super.key});
@@ -8,22 +10,31 @@ class AddNoteButtonSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 32,
-          ),
-          CustomTextfield(
-            hint: 'Title',
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          CustomTextfield(
-            hint: 'Content',
-            maxline: 5,
-          )
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            CustomTextfield(
+              hint: 'Title',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextfield(
+              hint: 'Content',
+              maxline: 5,
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            CustonButton(),
+            SizedBox(
+              height: 24,
+            ),
+          ],
+        ),
       ),
     );
   }

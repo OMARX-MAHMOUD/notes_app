e91196594019f5ebde09c34200484dd8e15feb88
omar mahmoud <omarmahmoudazema@gmail.com>
2024-09-13 14:10:18 +0300

    CREATE editnote view

---
+++ lib/views/editnoteview.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/editnoteview_body.dart';
+
+class Editnoteview extends StatelessWidget {
+  const Editnoteview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: EditnoteviewBody());
+  }
+}

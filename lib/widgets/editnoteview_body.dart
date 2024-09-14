d0deba67ab9abb4b1156f623788350dacb5e11b8
omar mahmoud <omarmahmoudazema@gmail.com>
2024-09-14 16:57:39 +0300

    edite note view is done

--- lib/widgets/editnoteview_body.dart
+++ lib/widgets/editnoteview_body.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_textfield.dart';
+import 'package:notes_app/widgets/customappbar.dart';
 
 class EditnoteviewBody extends StatelessWidget {
   const EditnoteviewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 40,
+          ),
+          CustomAppBar(text: 'Edite Note ', icon: Icons.check),
+          SizedBox(
+            height: 10,
+          ),
+          CustomTextfield(hint: 'Title'),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextfield(
+            hint: 'Content',
+            maxline: 5,
+          )
+        ],
+      ),
+    );
   }
 }
